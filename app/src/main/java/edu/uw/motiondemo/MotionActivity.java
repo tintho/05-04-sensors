47dbc800ee905e9a868b05fe83ee41be2aef7bde
Tin Ho <tintho@uw.edu>
2016-05-04 12:20:41 -0700

    end of class

--- app/src/main/java/edu/uw/motiondemo/MotionActivity.java
+++ app/src/main/java/edu/uw/motiondemo/MotionActivity.java
@@ -1,15 +1,26 @@
 package edu.uw.motiondemo;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MotionActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MotionActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "Motion";
 
     private TextView txtX, txtY, txtZ;
 
+    private SensorManager sensorManager;
+
+    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +32,50 @@ public class MotionActivity extends AppCompatActivity {
         txtY = (TextView)findViewById(R.id.txt_y);
         txtZ = (TextView)findViewById(R.id.txt_z);
 
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for(Sensor s : sensors) {
+            Log.v(TAG, s.toString());
+        }
+
+        //sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        if(sensor == null) {
+            Log.v(TAG, "No sensor available");
+            finish();
+        }
 
     }
 
+    @Override
+    protected void onResume() {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL); //delay game to do 50 times per second
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this, sensor); //if error here, need listener
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values; //acceleration in three directions, forward side down
+        //for accelerator
+//        txtX.setText(""+values[0]);
+//        txtY.setText(""+values[1]);
+//        txtZ.setText(""+values[2]);
+        txtX.setText(""+Math.toDegrees(values[0]));
+        txtY.setText(""+Math.toDegrees(values[1]));
+        txtZ.setText(""+Math.toDegrees(values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //when sensor changed to different sensor for different accuracy. accelerometer wont shift its accuracy level
+        //leave blank
+    }
 }
--- app/src/main/java/edu/uw/motiondemo/TouchActivity.java
+++ app/src/main/java/edu/uw/motiondemo/TouchActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 public class TouchActivity extends Activity {
@@ -12,6 +13,8 @@ public class TouchActivity extends Activity {
 
     private DrawingSurfaceView view;
 
+    private GestureDetector detector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,25 +22,81 @@ public class TouchActivity extends Activity {
 
         view = (DrawingSurfaceView)findViewById(R.id.drawingView);
 
+        detector =  new GestureDetector(this, new MyGestureListener());
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        //this is added
+        boolean guestured = detector.onTouchEvent(event);
+//        if(guestured) {
+//            return true;
+//        } we want to do other things
         float x = event.getX();
         float y = event.getY();
+
+        int pointerIndex = MotionEventCompat.getActionIndex(event);
+        int pointerId = MotionEventCompat.getPointerId(event, pointerIndex);
 
         //handle action
         int action = MotionEventCompat.getActionMasked(event);
         switch(action) {
             case MotionEvent.ACTION_DOWN:
                 Log.v(TAG,"Touch!");
-                return true;
 
+                view.addTouch(pointerId, event.getX(pointerIndex), event.getY(pointerIndex));
+
+                return true;
             case (MotionEvent.ACTION_MOVE) : //move
+
+                int pointerCount = event.getPointerCount();
+                for (int index = 0; index < pointerCount; index++) {
+                    int pId = event.getPointerId(index);
+                    view.moveTouch(pId, event.getX(index), event.getY(index));
+                }
+
+                return true;
+            case MotionEvent.ACTION_POINTER_DOWN:
+
+                view.addTouch(pointerId, event.getX(pointerIndex), event.getY(pointerIndex));
+
+                Log.v(TAG, "Another finger!");
+                return true;
+            case MotionEvent.ACTION_POINTER_UP:
+
+                view.removeTouch(pointerId);
+
+                Log.v(TAG, "A fingers left!");
+                return true;
+            case MotionEvent.ACTION_UP:
+
+                view.removeTouch(pointerId);
+
+                Log.v(TAG, "Last finger left");
                 return true;
             default :
                 return super.onTouchEvent(event);
+        }
+    }
+
+    public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        // this is specifically what we did in class
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true; //whether or not we handled the gesture //we handled this
+        }
+
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            //what do we want to do on fling
+            Log.v(TAG, "Fling!");
+            view.ball.dx = .03f*velocityX;
+            view.ball.dy = .03f*velocityY;
+
+            return true; //we handled this
         }
     }
 }
